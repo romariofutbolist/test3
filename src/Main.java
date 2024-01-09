6fd84e105928443eb930581d9b8b13e677eea320
romariofutbolist <66all@mail.ru>
2024-01-09 21:26:17 +0300

    сдача первого домашнего задания из третьего теста

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int sum = 0;
+        System.out.println(findValue(sum));
+
+    }
+    public static int findValue(int sum) {
+        Scanner scanner = new Scanner(System.in);
+        if(!scanner.hasNextInt()) {
+            System.out.println("Неправильно задано число");
+        }
+        int firstValue = scanner.nextInt();
+        if(!scanner.hasNextInt()) {
+            System.out.println("Неправильно задано число");
+        }
+        int secondValue = scanner.nextInt();
+        sum = firstValue+secondValue;
+        return sum;
     }
 }
+
+
